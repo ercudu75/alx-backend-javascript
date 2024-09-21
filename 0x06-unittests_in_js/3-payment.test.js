e2804b0867ca9f53e508203eb37fdcca6ed9c30c
ilyass <ilyass.bentalha2@gmail.com>
2024-09-21 01:23:44 +0100

    add tasks 0->4

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils.js');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should use Utils.calculateNumber', function () {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(spy, 'SUM', 100, 20);
+    spy.restore();
+  });
+});
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./4-payment.js');
+const Utils = require('./utils.js');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should use Utils.calculateNumber', function () {
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.returns(10);
+    const spy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledWith(stub, 'SUM', 100, 20);
+    sinon.assert.calledWith(spy, 'The total is: 10');
+
+    stub.restore();
+    spy.restore();
+  });
+});
