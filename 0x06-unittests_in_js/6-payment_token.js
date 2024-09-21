b8b63bf09f08b1360ac940d7df60e63f63f81776
ilyass <ilyass.bentalha2@gmail.com>
2024-09-21 01:24:38 +0100

    add tasks 5->7

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+function getPaymentTokenFromAPI(success) {
+  return new Promise((resolve) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    }
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
